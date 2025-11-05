2cfe12becd8b8146b46b628aed2e6c2bf487dffa
teslavmd <matuashaha@gmail.com>
2025-11-05 10:14:37 -0300

    feat: initial Spring-Boot project structure

---
+++ src/main/java/com/tpintegrador/IntegradorPa1Application.java
@@ -0,0 +1,13 @@
+package com.tpintegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorPa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradorPa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpintegrador/IntegradorPa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpintegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorPa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
